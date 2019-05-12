e46703644ba3d03300b7bccca2ae107be797c77b
sfanpro <st.sergeyf@gmail.com>
2019-05-12 14:03:18 +0300

    First of commit

---
+++ Fanblog/src/main/java/org/sfanpro/blog/FanblogApplication.java
@@ -0,0 +1,13 @@
+package org.sfanpro.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanblogApplication.class, args);
+	}
+
+}
